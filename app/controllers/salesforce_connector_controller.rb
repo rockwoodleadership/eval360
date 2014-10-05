8eec7d98adcb3d9c4854fa8bec57acc7aafb74eb
Diyah <me@hadiyah.me>
2014-10-05 12:09:35 -0700

    add test for invalid params for salesforce new participant

--- app/controllers/salesforce_connector_controller.rb
+++ app/controllers/salesforce_connector_controller.rb
@@ -1,12 +1,17 @@
 class SalesforceConnectorController < ApplicationController
   protect_from_forgery with: :null_session
   def new_participant
-    participant = JSON.parse(params[:participant])
 
-    attributes = { first_name: participant['First_Name__c'],
-                    last_name: participant['Last_Name__c'],
-                    email: participant['Email__c'] }
-    Participant.create(attributes)
-    render json: 'success', status: 200
+    if params[:participant] && params[:participant].is_a?(String) 
+      participant = JSON.parse(params[:participant])
+
+      attributes = { first_name: participant['First_Name__c'],
+                      last_name: participant['Last_Name__c'],
+                      email: participant['Email__c'] }
+      Participant.create(attributes)
+      render json: 'success', status: 200
+    else
+      render json: 'invalid params', status: 422
+    end
   end
 end
--- spec/controllers/salesforce_connector_controller_spec.rb
+++ spec/controllers/salesforce_connector_controller_spec.rb
@@ -4,16 +4,34 @@ RSpec.describe SalesforceConnectorController, :type => :controller do
 
   describe "POST new_particpant" do
 
-    it "creates a new participant" do
-      participant = { first_name: "Doris", last_name: "Day", email: "day@example.com" }
-      expect(Participant).to receive(:create).with(participant)
-      post :new_participant, :participant => participant
-    end 
+    context 'valid parameters' do
+
+      before(:each) do
+        @participant = JSON.generate({ First_Name__c: 'Doris', Last_Name__c: 'Day', Email__c: 'day@example.com' })
+        @attributes = {:first_name=>"Doris", :last_name=>"Day", :email=>"day@example.com"}
+      end 
+      
+      it "creates a new participant" do
+        expect(Participant).to receive(:create).with(@attributes)
+        post :new_participant, :participant => @participant
+      end 
 
 
-    it "returns status code 200" do
-      post :new_participant
-      expect(response.status).to eq 200
+      it "returns status code 200" do
+        post :new_participant, :participant => @participant
+        expect(response.status).to eq 200
+      end
+
     end
+
+    context 'invalid parameters' do
+
+      it "returns status code 422" do
+        post :new_participant
+        expect(response.status).to eq 422
+      end
+
+    end
+
   end
 end
