75768d8e637c0af163437997a582987319b54ca7
Diyah <me@hadiyah.me>
2014-10-26 22:20:19 -0700

    remove validate presence of questionnaire in program

--- spec/models/program_spec.rb
+++ spec/models/program_spec.rb
@@ -5,5 +5,4 @@ RSpec.describe Program, :type => :model do
   expect_it { to have_one :questionnaire }
   expect_it { to validate_presence_of :name }
   expect_it { to validate_uniqueness_of :name }
-  expect_it { to validate_presence_of :questionnaire }
 end
