d7a2cb1169033204915343dabac88d3439c8ce35
Diyah <me@hadiyah.me>
2014-10-26 19:46:19 -0700

    refactor evaluation js

--- app/assets/javascripts/evaluations.js
+++ app/assets/javascripts/evaluations.js
@@ -1,7 +1,6 @@
 $("#evaluation-edit input[type='range']").after("<label class='output'></label>");
 $("#evaluation-edit input[type='range']").after("Almost Always");
 $("#evaluation-edit input[type='range']").before("<br>Not Applicable");
-$input = $("input[type='range']");
 var rangeValues = {
     "0": "Not Applicable",
     "1": "1 - Almost Never",
@@ -15,41 +14,39 @@ var rangeValues = {
     "9": "9",
     "10": "10 - Almost Always"
 };
-$('#evaluation-edit .output').each(function(index) {
+
+$('#evaluation-edit .output').each(function() {
   $(this).text(rangeValues[$(this).siblings("input[type='range']")[0].value]);
 });
 
 $("#evaluation-edit input[type='range']").on('change', function () {
   $(this).siblings("label.output")[0].innerText = rangeValues[$(this).val()];
-
-});
-
-$("#evaluation-edit input[type='range']").on('change', function() {
+  var numRegEx, base, data, answer, answer_id;
   numRegEx = /\[numeric_response\]/;
-  name = $(this).attr('name');
-  base = name.split(numRegEx);
+  base = $(this).attr('name').split(numRegEx);
   data = {};
   answer = {};
   
-  answer["numeric_response"] = $(this).val();
+  answer.numeric_response = $(this).val();
   
-  data["answer"] = answer
+  data.answer = answer;
 
   answer_id = $("input[name='" + base[0] + "[id]'").val();
 
   $.post("/answers/"+ answer_id +"/update", data);
+
 });
 
 $("#evaluation-edit textarea").on('change', function() {
+  var textRegEx, base, data, answer, answer_id;
   textRegEx = /\[text_response\]/;
-  name = $(this).attr('name');
-  base = name.split(textRegEx);
+  base = $(this).attr('name').split(textRegEx);
   data = {};
   answer = {};
   
-  answer["text_response"] = $(this).val();
+  answer.text_response = $(this).val();
   
-  data["answer"] = answer
+  data.answer = answer
 
   answer_id = $("input[name='" + base[0] + "[id]'").val();
 
