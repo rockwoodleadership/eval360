6cb18a7e91889115f6bb040a6ce107927f2f837a
Diyah <me@hadiyah.me>
2014-11-13 14:10:56 -0800

    remove logout from pages controller

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,11 +6,6 @@ class PagesController < ApplicationController
     @me = me if authenticated?
   end
 
-  def logout
-    client.logout if authenticated?
-    render 'index'
-  end
-
   def thank_you
     render 'thank_you'
   end
