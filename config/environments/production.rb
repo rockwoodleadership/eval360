f0f5779a13e06641f2c4e24518fa5411e31a141f
Diyah <me@hadiyah.me>
2014-11-25 09:58:31 -0800

    troubleshooting send pw reset

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,7 +76,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  #config.action_mailer.default_url_options = { host: 'staging-rockwood.herokuapp.com', port: 3000 }
+  config.action_mailer.default_url_options = { host: 'staging-rockwood.herokuapp.com', port: 3000 }
 
   config.action_mailer.smtp_settings = {
     :address   => "smtp.mandrillapp.com",
