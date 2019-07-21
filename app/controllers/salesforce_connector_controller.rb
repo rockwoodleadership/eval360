786d0496aeabf56e57c48af90c87faadb766e1d9
Rockwood Developer <rockwoodengineer@users.noreply.github.com>
2019-07-21 15:03:07 -0700

    removing preferred_name ref..
    erence. See alterations on a different branch.

--- app/controllers/salesforce_connector_controller.rb
+++ app/controllers/salesforce_connector_controller.rb
@@ -5,11 +5,8 @@ class SalesforceConnectorController < ApplicationController
   def new_participant
     hash = JSON.parse(params[:participant])
 
-    required_keys = ['preferred_name', 'last_name', 'email',
+    required_keys = ['first_name', 'last_name', 'email',
                      'sf_training_id', 'sf_registration_id', 'sf_contact_id']
-
-    #removing 'first_name' from required keys in line 8, replacing with 'preferred_name'
-
     check_for_keys(required_keys, hash); return if performed?
     
     sf_training_id = hash['sf_training_id'] 
@@ -18,16 +15,9 @@ class SalesforceConnectorController < ApplicationController
     render json: 'invalid training record',
       status: 422 and return if training.nil?
 
-    attributes = hash.extract!('last_name', 'email',
+    attributes = hash.extract!('first_name', 'last_name', 'email',
                                'sf_registration_id', 'sf_contact_id')
-  
-    #remove 'first_name' from hash extraction in above line (19) and replace with  code below
-    attributes['first_name'] = hash.extract!('preferred_name') 
-    #if the preferred name is given, (which it will be b/c if it's not given then Form Assembly
-    #pushed preferred name to first name), set the attribute of first name equal to the extracted 
-    #value of preferred name. 
-
-
+ 
     existing = Participant.where(sf_contact_id: attributes['sf_contact_id'],
                                  sf_registration_id: attributes['sf_registration_id'])
     render json: 'participant already exists',
@@ -87,9 +77,8 @@ class SalesforceConnectorController < ApplicationController
 
     participants = Participant.where(sf_contact_id: hash['sf_contact_id'])
     attributes = {}
-    approved_fields = ['preferred_name', 'last_name', 'email',
+    approved_fields = ['first_name', 'last_name', 'email',
                                'sf_registration_id', 'sf_contact_id']
-    #removing 'first_name' from approved fields in line 90 and replacing with 'preferred_name'
 
     hash['changed_fields'].each do |cf|
       if cf == 'sf_training_id'
