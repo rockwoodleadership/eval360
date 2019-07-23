ceb6d68b26f66ff9725c6a84b71254bd346eede7
Rockwood Developer <rockwoodengineer@users.noreply.github.com>
2019-07-22 17:18:48 -0700

    Reverting Date.current to Date.today

--- app/models/evaluation.rb
+++ app/models/evaluation.rb
@@ -46,7 +46,8 @@ class Evaluation < ActiveRecord::Base
   end
 
   def not_accessible?
-    if self.evaluator.declined? or Date.current > participant.training.end_date
+    if self.evaluator.declined? or Date.today > participant.training.end_date
+      ##reverting Date.current to Date.today
       return true
     end
     return false
