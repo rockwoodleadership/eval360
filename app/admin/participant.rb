f6e7eee5457c81bc302f5e3da3d9da87297efcd5
Rockwood Developer <rockwoodengineer@users.noreply.github.com>
2019-11-19 12:35:18 -0800

    Update dropdown and actions in participant.rb

--- app/admin/participant.rb
+++ app/admin/participant.rb
@@ -71,10 +71,8 @@ ActiveAdmin.register Participant do
       input :first_name
       input :last_name
       input :email
-      input :training, 
-                 :as => :select, 
-                 :collection => Training.all
-    
+      input :training
+    end
     actions
   end
 
