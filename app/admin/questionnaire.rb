1283d837f2ea2d191ac38852b1eeaf1f00310623
Rockwood Developer <rockwoodengineer@users.noreply.github.com>
2021-02-01 09:20:02 -0800

    Update questionnaire.rb

--- app/admin/questionnaire.rb
+++ app/admin/questionnaire.rb
@@ -49,10 +49,6 @@ ActiveAdmin.register Questionnaire, as: "Assessment" do
       end
     end
 
-
-  div do
-    link_to('Trainings Associated with Assessment', admin_questionnaire_trainings_path(questionnaire))
-  end
 end
 
 end
